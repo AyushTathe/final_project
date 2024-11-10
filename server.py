c79aac5ef2254d99165d8d237f4155d4f2ac090a
AyushTathe <ayushtathe88@gmail.com>
2024-11-10 01:38:28 -0500

    Reached till  deployement

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emotion_detector_function():
+    test_to_analyze = request.args.get('textToAnalyze')
+    if not test_to_analyze:
+        return "Error: No text provided for analysis.", 400  # Simple check for empty input
+    response = emotion_detector(text_to_analyze)
+    print(response)  # Debugging output to check response
+    response_text = f"For the given statement, the system response is 'anger': \
+                    {response['anger']}, 'disgust': {response['disgust']}, \
+                    'fear': {response['fear']}, 'joy': {response['joy']}, \
+                    'sadness': {response['sadness']}. The dominant emotion is \
+                    {response['dominant_emotion']}."
+    return response_text
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
